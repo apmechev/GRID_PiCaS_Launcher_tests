63879cdf900da4f89c0c40978484a4db125dff7d
apmechev <apmechev@gmail.com>
2019-09-20 12:19:14 +0200

    Adding a helper function to increment version numbers

---
+++ increment_version.sh
@@ -0,0 +1,82 @@
+# Taken from https://stackoverflow.com/a/17364637 with many thanks
+# Accepts a version string and prints it incremented by one.
+# Usage: increment_version <version> [<position>] [<leftmost>]
+increment_version_rc() {
+	base=${1%rc[0-9]*}
+	export rc_version=$( echo $1 |sed -e "s/^v[0-9.]*rc//"  )	
+	rc_version_new=$( increment_version_normal $rc_version )
+	echo "${base}rc${rc_version_new}"
+	return 0
+}
+
+increment_version(){
+	if [[ ! -z $( echo $1 | grep rc ) ]]; 
+	then 
+		increment_version_rc $1;
+	else
+		increment_version_normal $1;
+	fi
+}
+
+
+increment_version_normal() {
+   local usage=" USAGE: $FUNCNAME [-l] [-t] <version> [<position>] [<leftmost>]
+           -l : remove leading zeros
+           -t : drop trailing zeros
+    <version> : The version string.
+   <position> : Optional. The position (starting with one) of the number 
+                within <version> to increment.  If the position does not 
+                exist, it will be created.  Defaults to last position.
+   <leftmost> : The leftmost position that can be incremented.  If does not
+                exist, position will be created.  This right-padding will
+                occur even to right of <position>, unless passed the -t flag."
+
+   # Get flags.
+   local flag_remove_leading_zeros=0
+   local flag_drop_trailing_zeros=0
+   while [ "${1:0:1}" == "-" ]; do
+      if [ "$1" == "--" ]; then shift; break
+      elif [ "$1" == "-l" ]; then flag_remove_leading_zeros=1
+      elif [ "$1" == "-t" ]; then flag_drop_trailing_zeros=1
+      else echo -e "Invalid flag: ${1}\n$usage"; return 1; fi
+      shift; done
+
+   # Get arguments.
+   if [ ${#@} -lt 1 ]; then echo "$usage"; return 1; fi
+   local v="${1}"             # version string
+   local targetPos=${2-last}  # target position
+   local minPos=${3-${2-0}}   # minimum position
+
+   # Split version string into array using its periods. 
+   local IFSbak; IFSbak=IFS; IFS='.' # IFS restored at end of func to                     
+   read -ra v <<< "$v"               #  avoid breaking other scripts.
+
+   # Determine target position.
+   if [ "${targetPos}" == "last" ]; then 
+      if [ "${minPos}" == "last" ]; then minPos=0; fi
+      targetPos=$((${#v[@]}>${minPos}?${#v[@]}:$minPos)); fi
+   if [[ ! ${targetPos} -gt 0 ]]; then
+      echo -e "Invalid position: '$targetPos'\n$usage"; return 1; fi
+   (( targetPos--  )) || true # offset to match array index
+
+   # Make sure minPosition exists.
+   while [ ${#v[@]} -lt ${minPos} ]; do v+=("0"); done;
+
+   # Increment target position.
+   v[$targetPos]=`printf %0${#v[$targetPos]}d $((10#${v[$targetPos]}+1))`;
+
+   # Remove leading zeros, if -l flag passed.
+   if [ $flag_remove_leading_zeros == 1 ]; then
+      for (( pos=0; $pos<${#v[@]}; pos++ )); do
+         v[$pos]=$((${v[$pos]}*1)); done; fi
+
+   # If targetPosition was not at end of array, reset following positions to
+   #   zero (or remove them if -t flag was passed).
+   if [[ ${flag_drop_trailing_zeros} -eq "1" ]]; then
+        for (( p=$((${#v[@]}-1)); $p>$targetPos; p-- )); do unset v[$p]; done
+   else for (( p=$((${#v[@]}-1)); $p>$targetPos; p-- )); do v[$p]=0; done; fi
+
+   echo "${v[*]}"
+   IFS=IFSbak
+   return 0
+}
